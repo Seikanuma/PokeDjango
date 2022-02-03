721add43a1f11939a33a3550aa8542d6dfe4aa65
jordan.servant@yahoo.fr <jordan.servant@yahoo.fr>
2022-02-04 00:51:58 +0100

    yey

--- PokeDjango/manage.py
+++ PokeDjango/manage.py
@@ -5,8 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PokeDjango.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokedex.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
